37fb81d62b0571270fb3e5dd4517b91cfb0a23ec
Jason Jimenez <jasonjimenez@live.com>
2024-11-11 16:45:02 +0800

    chore: setup opensass font and base sass properties

---
+++ ui/fonts.js
@@ -0,0 +1,8 @@
+import { Inter, Lato, Open_Sans } from "next/font/google";
+
+export const inter = Inter({ variable: "--font-inter" });
+export const lato = Lato({ variable: "--font-lato", weight: ["400", "700"] });
+export const openSans = Open_Sans({
+  variable: "--font-opensans",
+  weight: ["300", "400", "600"],
+});
